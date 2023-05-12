0bfd8f265935ec1cc647685c4c4dbff3ff5f8ef3
lanss-id <maulanakayyis354@gmail.com>
2023-05-12 14:00:42 +0700

    pertama

---
+++ app.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav
+      class="navbar fixed top-0 bg-[#72c9c9] z-10 backdrop-filter backdrop-blur-lg bg-opacity-50"
+    >
+      <a href="#" class="font-semibold text-xl px-7 text-white">Juke Bootcamp</a>
+    </nav>
+    <section class="hero min-h-screen bg-[#062534] bg-[url('/4.svg')] text-white">
+      <div class="hero-content flex-col lg:flex-row space-y-7">
+        <div class="mx-auto px-7">
+          <h1 class="text-5xl font-bold">
+            Layouting
+            <span
+              class="bg-clip-text text-transparent bg-gradient-to-r from-accent to-accent-focus"
+              >Tailwind CSS</span
+            >
+          </h1>
+          <p class="py-6">
+            Hai, <span class="font-bold"> Alan disini👋,</span> Saya mencoba
+            buat web sederhana bertema Layouting Tailwind CSS.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="bg-[#72c9c9] text-white">
+      <h1 class="text-center text-2xl font-bold tracking-wide pt-12 ">Animasi</h1>
+      <h1 class="text-center text-2xl font-bold">Kartu Skeleton</h1>
+      <div class="container px-6 py-10 mx-auto animate-pulse">
+          <div class="grid grid-cols-1 gap-8 mt-4 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+  
+              <div class="w-full ">
+                  <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600"></div>
+                  
+                  <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
